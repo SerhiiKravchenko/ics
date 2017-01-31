3200b014921cb1fbfc561d5d3986e802c73263d6
SerhiiKravchenko <srh.kravchenko@gmail.com>
2017-01-31 17:10:13 +0200

    edit file for warnings

--- index.js
+++ index.js
@@ -1,49 +1,8 @@
 
-
 let req = new XMLHttpRequest();
 req.open('GET', 'http://javascript.kiev.ua/attach/icalendar/google_events.json', false);
 req.send();
-if (req.status !== 200) {
-    console.log( req.status + ': ' + req.statusText );
-} else {
-    let parsedData = JSON.parse(req.responseText);
-    let calendar = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:s.kravchenko.org.ua\n";
-    parsedData.forEach(function(element, i, arr) {
-       calendar+=("BEGIN:VEVENT\nSUMMARY:"+textSplit(element.title)+"\nLOCATION:"+textSplit(element.location)+"\nURL:"
-           +textSplit(element.url)+"\nDTSTAMP:"+dateDtstamp()+"\nDTSTART:"+dateIcs(element.start)+"\nDTEND:"+dateIcs(element.end)
-            +"\nUID:"+element.id+"\nEND:VEVENT\n");
-        if(i == arr.length-1){calendar+="END:VCALENDAR\n"}
-    });
-    function textSplit(string) {
-        if (string.byteLength>60){
-            let stringNew = string;
-            let tabs = "\t";
-            let stringEnd = "";
-            for (let i=0; i=stringNew.length-1; i++){
-                let numb = 0;
-                let stringTmp = stringNew.slice(numb,i+1);
-                if(stringTmp.byteLength==60){
-                    numb = i;
-                    stringEnd+=tabs+stringTmp+"\n";
-                    tabs+="\t";
-                }else break;
-            }
-
-        } else return string;
-    }
-    function dateIcs(string){
-        let str = string.replace(/[^a-zA-Z0-9]/g, '').slice(0,-4);
-        if(str.length<15){
-            str+=0
-        }else return str;
-
-    }
-    function dateDtstamp(){
-        let dateStamp = new Date();
-        return dateIcs(dateStamp.toISOString());
-
-    }
-    String.prototype.byteLength = function(){
+String.prototype.byteLength = function(){
         let str = this;
         let length = str.length;
         let count = 0, i = 0, ch = 0;
@@ -65,6 +24,50 @@ if (req.status !== 200) {
         }
         return count;
     };
+if (req.status !== 200) {
+    console.log( req.status + ': ' + req.statusText );
+} else {
+    let parsedData = JSON.parse(req.responseText);
+    let calendar = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:s.kravchenko.org.ua\n";
+    parsedData.forEach(function(element, i, arr) {
+       calendar+=("BEGIN:VEVENT\nSUMMARY:"+textSplit(element.title)+"\nLOCATION:"+textSplit(element.location)+"\nURL:"
+           +textSplit(element.url)+"\nDTSTAMP:"+dateDtstamp()+"\nDTSTART:"+dateIcs(element.start)+"\nDTEND:"+dateIcs(element.end)
+            +"\nUID:"+element.id+"\nEND:VEVENT\n");
+        if(i == arr.length-1){calendar+="END:VCALENDAR\n"}
+    });
+	
+    function textSplit(string) {
+        if ((string.byteLength())>60){		
+		let space = " ";
+		let newString = "";
+		let count = 60;
+		for (i=0; i<string.length; i++){
+			let symbol = string.charAt(i);	
+			if ((newString.byteLength()+symbol.byteLength()) < (count-space.byteLength())){
+				newString+=symbol;
+			}else {
+				newString+="\n"+ space;
+				space+=" ";
+				count+=60;
+			}	
+		}
+		return newString;
+		
+        } else return string;
+    }
+    function dateIcs(string){
+        let str = string.replace(/[^a-zA-Z0-9]/g, '').slice(0,-4);
+        if(str.length<15){
+            str+=0
+        }else return str;
+
+    }
+    function dateDtstamp(){
+        let dateStamp = new Date();
+        return dateIcs(dateStamp.toISOString());
+
+    }
+    
 
 
 
@@ -75,4 +78,6 @@ if (req.status !== 200) {
 
 
 }
+
+
 
